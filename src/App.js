3aa5f3c678b48b76681a1a119eebec9181a7d69d
Gabarros <gabarros17@gmail.com>
2020-12-05 14:48:00 -0300

    refactoring of renders tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 
 function App() {
-  return <div  data-test="component-app" className="App"></div>;
+  return <div  data-test="component-app" className="App">
+    <h1 data-test="counter-display"> The Counter is currently: </h1>
+    <button data-test="increment-button">Increments Counter</button>
+  </div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import Enzyme, { shallow} from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-import App from './App';
+import App from "./App";
 
-Enzyme.configure({ adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
+
+/**
+ * Factory function to create a Shallow wrapper for the app component.
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
   expect(appComponent).toExist;
-
 });
 
 test("renders a button", () => {
-
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
-
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 });
 
-test("Counters starts at 0", () => {
+test("Counters starts at 0", () => {});
 
-});
-
-test("clicking a button increments counter display", () => {
-
-});
+test("clicking a button increments counter display", () => {});
